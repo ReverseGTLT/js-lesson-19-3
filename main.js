c0f96edc0891831a58453fa5f84487693a8021b9
ReverseGTLT <dzewrpifzc@gmail.com>
2022-12-14 20:01:22 +0200

    finish

--- main.js
+++ main.js
@@ -1,24 +1,203 @@
-const goodsFirst = document.querySelector('.goods--first');
-const goodsSecond = document.querySelector('.goods--second');
-const goodsThird = document.querySelector('.goods--third');
+const categories = [
+    {
+        id: 1,
+        category: "Toys",
+    },
+    {
+        id: 2,
+        category: "Sports",
+    },
+    {
+        id: 3,
+        category: "Toys",
+    },
+    {
+        id: 4,
+        category: "Grocery",
+    },
+    {
+        id: 5,
+        category: "Garden",
+    },
+];
 
-const titleFirst = document.querySelector('.list-categories__title--first');
-const titleSecond = document.querySelector('.list-categories__title--second');
-const titleThird = document.querySelector('.list-categories__title--third');
+const products = [
+    {
+        id: 1,
+        category_id: 2,
+        name: "Eggplant - Asian",
+        description:
+            "Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
+    },
+    {
+        id: 2,
+        category_id: 5,
+        name: "Jagermeister",
+        description:
+            "Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+    },
+    {
+        id: 3,
+        category_id: 2,
+        name: "Wine - Lou Black Shiraz",
+        description:
+            "Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.",
+    },
 
+    {
+        id: 4,
+        category_id: 3,
+        name: "Beef - Ox Tongue",
+        description:
+            "Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
+    },
+    {
+        id: 5,
+        category_id: 3,
+        name: "Beef - Bones, Marrow",
+        description:
+            "Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.",
+    },
+    {
+        id: 6,
+        category_id: 2,
+        name: "Appetizer - Shrimp Puff",
+        description:
+            "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus.",
+    },
+    {
+        id: 7,
+        category_id: 4,
+        name: "Appetizer - Shrimp Puff",
+        description:
+            "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+    },
+    {
+        id: 8,
+        category_id: 1,
+        name: "Ecolab - Ster Bac",
+        description: "Integer ac leo. Pellentesque ultrices mattis odio.",
+    },
+    {
+        id: 9,
+        category_id: 4,
+        name: "Vegetable - Base",
+        description:
+            "Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam.",
+    },
+    {
+        id: 10,
+        category_id: 3,
+        name: "Flour - Corn, Fine",
+        description:
+            "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat.",
+    },
+    {
+        id: 11,
+        category_id: 1,
+        name: "Beer - Guiness",
+        description:
+            "In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+    },
+    {
+        id: 12,
+        category_id: 4,
+        name: "Napkin White",
+        description:
+            "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+    },
+    {
+        id: 13,
+        category_id: 2,
+        name: "Oil - Truffle, White",
+        description:
+            "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+    },
+    {
+        id: 14,
+        category_id: 5,
+        name: "Wine - Puligny Montrachet A.",
+        description:
+            "Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat.",
+    },
+    {
+        id: 15,
+        category_id: 5,
+        name: "Tomatoes Tear Drop",
+        description:
+            "Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh. Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est.",
+    },
+];
 
-function showFirst(e){
-    e.addEventListener('click', () => {
-        if (e === titleFirst) {
-            goodsFirst.classList.toggle('visible')
-        } else if (e === titleSecond) {
-            goodsSecond.classList.toggle('visible')
-        } else if (e === titleThird) {
-            goodsThird.classList.toggle('visible')
-        }
-    })
+const container = document.querySelector(".container");
+const productsUl = document.querySelector("#products");
+const description = document.querySelector("#description");
+const buyButton = document.querySelector("#buy-button");
+
+container.addEventListener("click", (e) => {
+    const target = e.target;
+    let productsByCategory;
+
+    if (target.closest("#categories")) {
+        showProductList(productsByCategory, target);
+    }
+
+    if (target.closest("#products")) {
+        showProductInfo(target, products);
+    }
+
+    if (target.closest("#buy-button")) {
+        returnInitialState();
+    }
+});
+
+function createArrayByCategory(categoryId) {
+    const arrayCategory = products.filter((el) => {
+        return el.category_id === categoryId;
+    });
+    return arrayCategory;
 }
 
-showFirst(titleFirst);
-showFirst(titleSecond);
-showFirst(titleThird);
+function showProductList(list, target) {
+    list = createArrayByCategory(Number(target.dataset.categoryId));
+    let productsList = productsUl.querySelectorAll("li");
+
+    if (list.length > productsList.length) {
+        const countIteration = list.length - productsList.length;
+
+        for (let i = 0; i < countIteration; i++) {
+            const li = document.createElement("li");
+            productsUl.appendChild(li);
+            console.log(productsUl.length);
+        }
+        productsList = productsUl.querySelectorAll("li");
+    }
+
+    productsList.forEach((el) => {
+        el.innerText = "";
+    });
+
+    list.forEach((el, index) => {
+        productsList[index].innerText = el.name;
+    });
+
+    productsUl.style.visibility = "visible";
+}
+
+function showProductInfo(target, listProduct) {
+    const product = listProduct.find((el) => el.name === target.innerText);
+    description.innerText = product.description;
+    description.style.visibility = "visible";
+    buyButton.style.visibility = "visible";
+}
+
+function returnInitialState() {
+    const message = document.querySelector("#message");
+    message.style.visibility = "visible";
+    setTimeout(() => {
+        message.style.visibility = "hidden";
+        buyButton.style.visibility = "hidden";
+        description.style.visibility = "hidden";
+        productsUl.style.visibility = "hidden";
+    }, 1000);
+}
